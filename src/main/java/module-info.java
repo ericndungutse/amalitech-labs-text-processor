6f4c5cfd8513537ac4dff4928e9abbc90a199cb1
ericndungutse <dav.ndungutse@gmail.com>
2025-05-02 10:02:51 +0200

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.ndungutse.text_processor {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.ndungutse.text_processor to javafx.fxml;
+    exports org.ndungutse.text_processor;
+}
