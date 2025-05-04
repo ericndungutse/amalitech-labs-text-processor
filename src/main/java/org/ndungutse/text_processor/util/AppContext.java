9342f71cc3b74920791313356062e037cb19ade8
ericndungutse <dav.ndungutse@gmail.com>
2025-05-04 21:06:56 +0200

    feat: log every process as files are being proccessed on the ui

--- src/main/java/org/ndungutse/text_processor/util/AppContext.java
+++ src/main/java/org/ndungutse/text_processor/util/AppContext.java
@@ -1,16 +1,31 @@
 package org.ndungutse.text_processor.util;
 
 import org.ndungutse.text_processor.service.RegexService;
+import javafx.scene.control.TextArea;
 
 public class AppContext {
     private static RegexService regexService;
+    private static TextArea logTextArea; // Static TextArea to access it globally
 
     private AppContext() {
     }
+
     public synchronized static RegexService getRegexService() {
         if (regexService == null) {
             regexService = new RegexService();
         }
         return regexService;
+    }
+
+    public static void setLogTextArea(TextArea logTextArea) {
+        if (AppContext.logTextArea == null) {
+            AppContext.logTextArea = logTextArea;
+        }
+    }
+
+    public static void log(String message) {
+        if (logTextArea != null) {
+            logTextArea.appendText(message + "\n");
+        }
     }
 }
